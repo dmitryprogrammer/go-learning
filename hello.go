6ba3e8528c2e466746a69170b0d25eb9d38539eb
CodeViking13 <92metaller@gmail.com>
2017-04-04 22:09:34 +0400

    I wrote loop and condition.

--- hello.go
+++ hello.go
@@ -9,9 +9,20 @@ func main() {
 	var s, sep string
 	var i, argsLen int
 	argsLen = len(os.Args)
-	for i = 1; i < argsLen; i++ {
-		s += sep + os.Args[i]
-		sep = " "
+
+	if os.Args[1] == "a" {
+		s = ""
+		sep = ""
+		for _, arg := range os.Args[1:] {
+			s += sep + arg
+			sep = " "
+		}
+		fmt.Println(s)
+	} else {
+		for i = 1; i < argsLen; i++ {
+			s += sep + os.Args[i]
+			sep = " "
+		}
+		fmt.Println(s)
 	}
-	fmt.Println(s)
 }
