705577cf10d78a18c7fea76326bcfab361b96106
CodeViking13 <92metaller@gmail.com>
2017-04-12 21:44:10 +0400

    new practice

--- hello.go
+++ hello.go
@@ -1,28 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"fmt"
 )
 
 func main() {
-	var s, sep string
-	var i, argsLen int
-	argsLen = len(os.Args)
-
-	if os.Args[1] == "a" {
-		s = ""
-		sep = ""
-		for _, arg := range os.Args[1:] {
-			s += sep + arg
-			sep = " "
-		}
-		fmt.Println(s)
-	} else {
-		for i = 1; i < argsLen; i++ {
-			s += sep + os.Args[i]
-			sep = " "
-		}
-		fmt.Println(s)
-	}
+	fmt.Print(len(os.Args))
 }
