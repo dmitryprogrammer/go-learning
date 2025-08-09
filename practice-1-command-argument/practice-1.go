0bf8793db8c58ffa5ba1569a920cc4095d909eda
dmitryprogrammer <dpischalka@gmail.com>
2025-08-09 23:53:00 +0300

    practice-1-command-argument: Created different functions with output argumends command

--- practice-1-command-argument/practice-1.go
+++ practice-1-command-argument/practice-1.go
@@ -3,13 +3,58 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+	rangeIteration()
+	indexedIteration(true)
+}
+
+// First implementation of go command arguments output iteration
+func simpleIteration() {
 	var arguments, sep string
 	for i := 1; i < len(os.Args); i++ {
 		arguments += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(arguments)
+}
+
+// Iteration implemented with range
+func rangeIteration() {
+	var arguments string
+	var sep = " "
+
+	for _, arg := range os.Args[1:] {
+		arguments += arg + sep
+	}
+	fmt.Println(arguments)
+}
+
+// It is not real iteration, but short case how it possible to output arguemnts	
+func joinIteration() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
+
+// Simpliest case how to output arugment
+func shortIteration() {
+	fmt.Println(os.Args[1:])
+}
+
+func indexedIteration(isFileVisible bool) {
+	var arguemnts string
+	var sep = " "
+	var startRange int8
+
+	if isFileVisible {
+		startRange = 0
+	} else {
+		startRange = 1
+	}
+
+	for _, arg:= range os.Args[startRange:] {
+		arguemnts += arg + sep
+	}
+	fmt.Println(arguemnts)
 }
