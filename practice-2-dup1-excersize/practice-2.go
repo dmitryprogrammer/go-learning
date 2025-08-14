be0e922910667d432e048846eb23916676384184
dmitryprogrammer <dpischalka@gmail.com>
2025-08-14 21:14:11 +0300

    practice-2-dup1-excersize: finished first version of practice 2.

--- practice-2-dup1-excersize/practice-2.go
+++ practice-2-dup1-excersize/practice-2.go
@@ -7,15 +7,17 @@ import (
 )
 
 func main() {
-	counts:= make(map[string]int);
-	input:= bufio.NewScanner(os.Stdout);
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		counts[input.Text()]++
 	}
 
-	for line, n:= range counts {
+	fmt.Printf("\n");
+
+	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line);
+			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 }
